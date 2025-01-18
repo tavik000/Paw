3f4841341b82667893d226996d64bfc9022858f0
tavik000 <tavik002@gmail.com>
2025-01-18 11:04:23 +0900

    source code

---
+++ Source/Paw/PawGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "PawGameMode.generated.h"
+
+UCLASS(minimalapi)
+class APawGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	APawGameMode();
+};
+
+
+
