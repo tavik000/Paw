ceb8a7836d13183381d5dc9522a2ab3c911faf34
tavik000 <tavik002@gmail.com>
2025-01-25 22:32:45 +0900

    Add bubble push static mesh and projecitle bubble self break

--- Source/Paw/Weapon/Projectile/PawProjectile_Bubble.cpp
+++ Source/Paw/Weapon/Projectile/PawProjectile_Bubble.cpp
@@ -3,6 +3,8 @@
 
 #include "PawProjectile_Bubble.h"
 
+#include "Components/SphereComponent.h"
+#include "Engine/AssetManager.h"
 #include "GameFramework/ProjectileMovementComponent.h"
 #include "Kismet/GameplayStatics.h"
 #include "Paw/Character/Player/PawCharacter.h"
@@ -12,17 +14,74 @@
 // Sets default values
 APawProjectile_Bubble::APawProjectile_Bubble()
 {
-	ProjectileMovement->bShouldBounce = false;
 	BubbleMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("BubbleMesh"));
 	BubbleMesh->SetupAttachment(RootComponent);
 	BubbleMesh->SetCollisionProfileName(TEXT("NoCollision"));
 	BubbleMesh->SetIsReplicated(true);
 	BubbleMesh->SetVisibility(true);
+
+	InitialLifeSpan = 0.0f;
 }
 
 void APawProjectile_Bubble::BeginPlay()
 {
 	Super::BeginPlay();
+	if (HasAuthority())
+	{
+		FTimerHandle TimerHandle;
+		GetWorld()->GetTimerManager().SetTimer(TimerHandle, this, &APawProjectile_Bubble::SelfBreak, BubbleLifeTime,
+		                                       false);
+	}
+	LoadBreakEffect();
+}
+
+void APawProjectile_Bubble::Break_Implementation()
+{
+	if (!HasAuthority())
+	{
+		return;
+	}
+	IPawCollideBreakableInterface::Break_Implementation();
+	MulticastSpawnBreakEffect();
+
+	// Wait 5 sec and destroy
+	FTimerHandle TimerHandle;
+	GetWorld()->GetTimerManager().SetTimer(TimerHandle, this, &APawProjectile_Bubble::SelfDestroy, 5.0f, false);
+	CollisionComp->SetCollisionEnabled(ECollisionEnabled::NoCollision);
+	BubbleMesh->SetVisibility(false);
+}
+
+void APawProjectile_Bubble::SelfBreak()
+{
+	if (IPawCollideBreakableInterface* CollideBreakableInterface = Cast<IPawCollideBreakableInterface>(this))
+	{
+		CollideBreakableInterface->Break_Implementation();
+	}
+}
+
+void APawProjectile_Bubble::LoadBreakEffect()
+{
+	UAssetManager::Get().GetStreamableManager().RequestAsyncLoad(BreakEffectAsset.ToSoftObjectPath(),
+	                                                             FStreamableDelegate::CreateUObject(
+		                                                             this,
+		                                                             &APawProjectile_Bubble::OnBreakEffectLoaded));
+}
+
+void APawProjectile_Bubble::OnBreakEffectLoaded()
+{
+}
+
+void APawProjectile_Bubble::MulticastSpawnBreakEffect_Implementation()
+{
+	if (IsValid(BreakEffectAsset.Get()))
+	{
+		BreakEffect = UNiagaraFunctionLibrary::SpawnSystemAtLocation(GetWorld(), BreakEffectAsset.Get(),
+		                                                             GetActorLocation(),
+		                                                             GetActorRotation(),
+		                                                             FVector::One() * BreakEffectScale, true, true,
+		                                                             ENCPoolMethod::AutoRelease,
+		                                                             true);
+	}
 }
 
 void APawProjectile_Bubble::Tick(float DeltaTime)
@@ -47,7 +106,12 @@ void APawProjectile_Bubble::OnHit(UPrimitiveComponent* HitComp, AActor* OtherAct
 	APawCharacter* HitHider = Cast<APawCharacter>(OtherActor);
 	if (!IsValid(HitHider))
 	{
-		Destroy();
+		// Only add impulse and destroy projectile if we hit a physics
+		if ((OtherActor != nullptr) && (OtherActor != this) && (OtherComp != nullptr) && OtherComp->
+			IsSimulatingPhysics())
+		{
+			OtherComp->AddImpulseAtLocation(GetVelocity() * PushForce, GetActorLocation());
+		}
 		return;
 	}
 
@@ -67,5 +131,14 @@ void APawProjectile_Bubble::OnHit(UPrimitiveComponent* HitComp, AActor* OtherAct
 		BubbleHiderCapture->ServerCaptureHider(HitHider);
 	}
 
+	Destroy();
+}
+
+void APawProjectile_Bubble::SelfDestroy()
+{
+	if (!HasAuthority())
+	{
+		return;
+	}
 	Destroy();
 }
