612602ea98263995dcc9890d06efc77a12bd492e
tavik000 <tavik002@gmail.com>
2025-07-05 23:33:51 +0900

    Convert Cat BP to cpp: create TPPlayer class

---
+++ Source/Paw/Character/Player/PawTPPlayer.cpp
@@ -0,0 +1,94 @@
+﻿// ©2025 Key. All rights reserved. "Project Paw" and all related assets, trademarks, and materials are the intellectual property of Key. Unauthorized reproduction, distribution, or use of any content from this game is strictly prohibited.
+
+#include "PawTPPlayer.h"
+
+#include "InputActionValue.h"
+#include "Camera/CameraComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
+
+APawTPPlayer::APawTPPlayer()
+{
+	// Don't rotate when the controller rotates. Let that just affect the camera.
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationYaw = false;
+	bUseControllerRotationRoll = false;
+
+	// Configure character movement for third person
+	GetCharacterMovement()->bOrientRotationToMovement = true; // Character moves in the direction of input...
+	GetCharacterMovement()->RotationRate = FRotator(0.0f, 500.0f, 0.0f); // ...at this rotation rate
+
+	// Create a camera boom (pulls in towards the player if there is a collision)
+	CameraBoom = CreateDefaultSubobject<USpringArmComponent>(TEXT("CameraBoom"));
+	CameraBoom->SetupAttachment(RootComponent);
+	CameraBoom->TargetArmLength = 400.0f; // The camera follows at this distance behind the character
+	CameraBoom->bUsePawnControlRotation = true; // Rotate the arm based on the controller
+
+	// Create a follow camera
+	ThirdPersonCameraComponent = CreateDefaultSubobject<UCameraComponent>(TEXT("ThirdPersonCamera"));
+	ThirdPersonCameraComponent->SetupAttachment(CameraBoom, USpringArmComponent::SocketName);
+	ThirdPersonCameraComponent->bUsePawnControlRotation = false; // Camera does not rotate relative to arm
+}
+
+void APawTPPlayer::BeginPlay()
+{
+	Super::BeginPlay();
+}
+
+void APawTPPlayer::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void APawTPPlayer::NotifyControllerChanged()
+{
+	Super::NotifyControllerChanged();
+}
+
+void APawTPPlayer::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
+void APawTPPlayer::Move(const FInputActionValue& Value)
+{
+	// Call parent first (in case there's base logic)
+	Super::Move(Value);
+
+	// input is a Vector2D
+	FVector2D MovementVector = Value.Get<FVector2D>();
+
+	if (Controller != nullptr)
+	{
+		// find out which way is forward
+		const FRotator Rotation = Controller->GetControlRotation();
+		const FRotator YawRotation(0, Rotation.Yaw, 0);
+
+		// get forward vector
+		const FVector ForwardDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+
+		// get right vector
+		const FVector RightDirection = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+
+		// add movement
+		AddMovementInput(ForwardDirection, MovementVector.Y);
+		AddMovementInput(RightDirection, MovementVector.X);
+	}
+}
+
+void APawTPPlayer::Look(const FInputActionValue& Value)
+{
+	// Call parent first (in case there's base logic)
+	Super::Look(Value);
+
+	// input is a Vector2D
+	FVector2D LookAxisVector = Value.Get<FVector2D>();
+
+	if (Controller != nullptr)
+	{
+		// add yaw and pitch input to controller
+		AddControllerYawInput(LookAxisVector.X);
+		AddControllerPitchInput(LookAxisVector.Y);
+	}
+}
---
+++ Source/Paw/Character/Player/PawTPPlayer.h
@@ -0,0 +1,42 @@
+﻿// ©2025 Key. All rights reserved. "Project Paw" and all related assets, trademarks, and materials are the intellectual property of Key. Unauthorized reproduction, distribution, or use of any content from this game is strictly prohibited.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PawPlayerBase.h"
+#include "GameFramework/SpringArmComponent.h"
+#include "PawTPPlayer.generated.h"
+
+class UCameraComponent;
+
+UCLASS()
+class PAW_API APawTPPlayer : public APawPlayerBase
+{
+	GENERATED_BODY()
+
+public:
+	APawTPPlayer();
+
+protected:
+	virtual void BeginPlay() override;
+	virtual void NotifyControllerChanged() override;
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+	virtual void Move(const FInputActionValue& Value) override;
+	virtual void Look(const FInputActionValue& Value) override;
+
+	// Third Person Camera Components
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	USpringArmComponent* CameraBoom;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+	UCameraComponent* ThirdPersonCameraComponent;
+
+public:
+	virtual void Tick(float DeltaTime) override;
+
+	UFUNCTION(BlueprintCallable)
+	UCameraComponent* GetThirdPersonCameraComponent() const { return ThirdPersonCameraComponent; }
+
+	UFUNCTION(BlueprintCallable)
+	USpringArmComponent* GetCameraBoom() const { return CameraBoom; }
+};
